0f36d81450d7e5ffd58a4f2a0406de87a783e6c5
Md. Shah Alam <shahalam141072@gmail.com>
2025-12-08 02:39:33 +0600

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,66 @@
-import Image from "next/image";
+import { MessageSquareDot } from "lucide-react";
+import Link from "next/link";
+
+const shortsLinks = [
+  "cimrHW31uMo",
+  "al4hTmrfmmM",
+  "sP9vK6v8V-M"
+];
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 bg-linear-to-br from-slate-50 to-slate-100 h-full md:h-screen">
+      <div className="w-full max-w-4xl space-y-4">
+        <h3 className="bangla-text text-center text-xl md:text-2xl text-slate-700 font-medium leading-relaxed">
+          আমাদের সাথে যোগাযোগ করতে চাইলে নিচের বাটন এ ক্লিক করুন
+        </h3>
+        {/* WhatsApp Contact Button */}
+        <div className="flex justify-center">
+          <Link
+            href={`https://wa.me/8801926859047`}
+            className="flex items-center gap-3 px-6 py-4 bg-green-500 hover:bg-green-600 rounded-2xl shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-200"
+          >
+            <MessageSquareDot className="text-white w-6 h-6" />
+            <span className="text-xl font-bold text-white">Chat on WhatsApp</span>
+          </Link>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+
+        {/* Bengali Text */}
+        <div className="text-center">
+          <h3 className="bangla-text text-xl md:text-2xl text-slate-700 font-medium leading-relaxed">
+            বিস্তারিত জানতে আমাদের সাথে সরাসরি কথা বলতে যোগাযোগ করুন
+          </h3>
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="tel:+8801926859047"
+            style={{ letterSpacing: 4 }}
+            className="text-3xl md:text-3xl font-bold text-green-600 hover:text-green-700 transition-colors"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+            01926-859047
           </a>
         </div>
-      </main>
+
+        {/* Video Section */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
+          {shortsLinks.length > 0 && (
+            shortsLinks.map((short: any, index: number) => (
+              <div
+                key={index}
+                className="relative w-full rounded-2xl overflow-hidden shadow-2xl bg-black"
+                style={{ aspectRatio: '9/16' }}
+              >
+                <iframe
+                  src={`https://www.youtube.com/embed/${short}`}
+                  className="absolute top-0 left-0 w-full h-full"
+                  allowFullScreen
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  title="Product Video"
+                ></iframe>
+              </div>
+            ))
+          )}
+        </div>
+
+      </div>
     </div>
   );
 }
